42ecad963c625b276cc6dc19bdf32079e04e3a28
Duli Chand Prajapati <duli.prajapati@infinitylabs.in>
2018-10-26 19:14:14 +0530

    CHanges

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 const mode = process.env.NODE_ENV || "development";
 var env = require('dotenv').config({
   path: `.env.${mode}`
@@ -20,16 +21,20 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'uploads')));
-app.use(function (req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  
-  next();
-});
+var whitelist = ['http://localhost:4200',"http://192.168.0.140:8083","http://192.168.0.148:8282"]
+ var corsOptions = {
+    origin: function (origin, callback) {
+      console.log(origin);
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        console.log("Success")
+        callback(null, true)
+      } else {
+        console.log('Not allowed by CORS')
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
+app.use(cors(corsOptions))
 require('./routes')(app);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
